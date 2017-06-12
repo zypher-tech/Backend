1bb45a1deb091c3c8f0898d2202a290e717adbe9
nanborklabs <nandhu12195@gmail.com>
2017-06-12 17:10:44 +0530

    API's in Routes

--- routes/home_api.js
+++ routes/home_api.js
@@ -6,15 +6,51 @@
 var express = require('express');
 var admin = require("firebase-admin");
 
+
+var response ;
+var returnJson = {
+	"combos":[],
+	"products":[]
+};
+
 // Get a database reference to our blog
 var db = admin.database();
 const router = express.Router();
 
 exports.initialize = function(req, res){
- var db = admin.database();
- var ref = db.ref("books");
+
+	response = res;
+	console.log("inside Home");
+ 	var db = admin.database();
+ 	var ref = db.ref("books");
 	ref.once("value", function(snapshot) {
-  		console.log('Value from books : '+snapshot.val());
-  		res.send(snapshot.val());
-	});
+
+  	  console.log("Value from Products Table "+ snapshot.val());
+  	  returnJson.products = snapshot.val();
+  
+	})
+	.then(getCombos,error);
+};
+
+function error(){
+	console.log("Error "+error);
+};
+
+
+
+function getCombos(snapshot){
+	console.log("Current Json "+returnJson);
+	var ref = db.ref("combos");
+	ref.once("value", function(snapshot) {
+
+  	  console.log("Value from Combos Table "+ snapshot.val());
+  	  returnJson.combos = snapshot.val();
+  
+	}).then(sendJson,error);
+};
+
+function sendJson(){
+	//get Individual Products
+	console.log("Final Json "+returnJson);
+	response.send(returnJson);
 };
