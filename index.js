285ec06f31127990a75fe8d40762a550a1fc8666
nanborklabs <nandhu12195@gmail.com>
2017-06-13 19:51:15 +0530

    Creating Cloud Functions - Moving index.js to root

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ exports.home_page =functions.https.onRequest((request, response) => {
  response.send("Hello from Home AppBackend");
 });
 
+exports.orderIsRecevied = functions.database.ref('/orders/{pushId}')
+    .onWrite(snapshot => {
+      // Grab the current value of what was written to the Realtime Database.
+      console.log("Order Received : "+snapshot.data.val());
+      
 
-
+    });
