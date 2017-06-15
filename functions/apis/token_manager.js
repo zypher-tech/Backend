9ae46d5e2ee7a45a92f12beea6b558c2b54bad83
nanborklabs <nandhu12195@gmail.com>
2017-06-16 01:50:00 +0530

    order->driver->Message

--- functions/apis/token_manager.js
+++ functions/apis/token_manager.js
@@ -28,6 +28,7 @@ exports.saveToken = function(req,res) {
  	 	}
  	 	else if(fromWhere == '2'){
  	 		//Riders
+			
 
  	 		saveRiderToken(receivedToken,profileId);
  	 	}
@@ -40,16 +41,16 @@ exports.saveToken = function(req,res) {
 function saveUserToken(receivedToken,profileId){
 	  console.log("Saving User Token");
 	  var usersRef  = db.ref("users");
-	  
+
 };
 
 function savePartnerToken(receivedToken,profileId){
 	  console.log("Saving User Token");
 	  var partnersRef  = db.ref("users");
-	  
+
 };
 function saveRiderToken(receivedToken,profileId){
 	  console.log("Saving User Token");
 	  var ridersRef  = db.ref("users");
-	  
+
 };
--- functions/apis/update_rider.js
+++ functions/apis/update_rider.js
@@ -22,7 +22,8 @@ exports.broadcastToRiders = function(order) {
   try {
     ridersRef.on("child_added",snap => {
         var riderName = snap.val().riderName;
-        console.log("Rider Name "+riderName);
+        var token = snap.val().tokeId;
+        pushNewOrder(token,order);
 
     });
 
@@ -31,4 +32,40 @@ exports.broadcastToRiders = function(order) {
   } finally {
 
   }
+  };
+
+
+  //Send as a Cloud Message to this Bucket Maybe--?
+  function pushNewOrder(tokenId,order){
+    var registrationTokens = tokenId;
+
+// See the "Defining the message payload" section below for details
+// on how to define a message payload.
+
+var payload = {
+data: {
+  type:'new_order' ,
+  orderId: order.orderId,
+  orderLat:order.orderLat,
+  orderLan:order.orderLon,
+  name : order.firstName + order.lastName,
+  phoneNumber:order.phoneNumber,
+  products:order.products,
+  amount:order.amount;
+  orderStatus:order.orderStatus;
+  elapsedBy:order.timingEngine.orderInsertedAt
+  }
+};
+
+// Send a message to the devices corresponding to the provided
+// registration tokens.
+admin.messaging().sendToDevice(registrationTokens, payload)
+.then(function(response) {
+  // See the MessagingDevicesResponse reference documentation for
+  // the contents of response.
+  console.log("Successfully sent message:", response);
+})
+.catch(function(error) {
+  console.log("Error sending message:", error);
+});
   };
