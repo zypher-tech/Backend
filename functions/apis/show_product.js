21078424a549b7600e56149a38e97ea935f8d082
nanborklabs <nandhu12195@gmail.com>
2017-06-15 18:24:00 +0530

    showing Product *C, push Notifications

---
+++ functions/apis/show_product.js
@@ -0,0 +1,37 @@
+
+'use strict';
+
+var express = require('express');
+var admin = require("firebase-admin");
+
+
+var db = admin.database();
+
+
+
+
+exports.sendProduct = function(req,res) {
+	// body...
+
+
+	//First Get the Pid that the user Wants to see
+	var pid = req.body.pid;
+
+		ordersRef.once("value")
+			.then(function (snapshot) {
+
+				//incremnet Count
+				snapshot.forEach(function (singleData) {
+						
+						if (pid == singleData.pid) {
+							//Matches
+							res.send(singleData);
+						}
+						else{
+							//DO Nothinh
+						}
+
+
+			 });
+			});
+};
---
+++ functions/apis/token_manager.js
@@ -0,0 +1,55 @@
+
+var express = require('express');
+var admin = require("firebase-admin");
+
+
+
+var failedStatus =  {
+
+	status : '0'
+
+};
+
+
+
+exports.saveToken = function(req,res) {
+
+	var fromWhere = req.body.fromWhere;
+	var receivedToked = req.body.tokenId;
+	var profileId = req.body.profileId;
+ 	 	if (fromWhere == '0') {
+ 	 		//From User
+			saveUserToken(receivedToked,profileId);
+ 	 	}
+ 	 	else if(fromWhere == '1'){
+ 	 		//from Partners
+
+ 	 		savePartnerToken(receivedToken,profileId);
+ 	 	}
+ 	 	else if(fromWhere == '2'){
+ 	 		//Riders
+
+ 	 		saveRiderToken(receivedToken,profileId);
+ 	 	}
+ 	 	else{
+ 	 			res.send(failedStatus);
+ 	 	}
+ };
+
+
+function saveUserToken(receivedToken,profileId){
+	  console.log("Saving User Token");
+	  var usersRef  = db.ref("users");
+	  
+};
+
+function savePartnerToken(receivedToken,profileId){
+	  console.log("Saving User Token");
+	  var partnersRef  = db.ref("users");
+	  
+};
+function saveRiderToken(receivedToken,profileId){
+	  console.log("Saving User Token");
+	  var ridersRef  = db.ref("users");
+	  
+};
